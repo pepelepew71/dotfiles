458852e936cf6c1a99341752ba727b78b0251933
yc.huang71 <yc.huang71@gmail.com>
2019-04-02 15:56:09 +0800

    ubuntu 18.04 gnome

--- backup.sh
+++ backup.sh
@@ -2,12 +2,6 @@
 
 cp ~/.vimrc       ./
 cp ~/.tmux.conf   ./
-cp ~/.Xresources  ./
 
-cp ~/.config/compton.conf   ./.config/
 cp ~/.config/redshift.conf  ./.config/
-cp ~/.config/i3             ./.config/  -r
-cp ~/.config/i3blocks       ./.config/  -r
-cp ~/.config/script         ./.config/  -r
-cp ~/.config/ranger/config  ./.config/ranger/
 
