fb17da2c1d031ac22a7431b9d2404563ae5ad858
yc.huang71 <yc.huang71@gmail.com>
2019-04-12 06:16:49 +0800

    ubuntu 16.04

---
+++ dconf/dump_dconf.sh
@@ -0,0 +1,3 @@
+dconf dump /org/gnome/desktop/wm/keybindings/ > wm_keys.dconf
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > media_keys.dconf
+
---
+++ dconf/load_dconf.sh
@@ -0,0 +1,3 @@
+dconf load /org/gnome/desktop/wm/keybindings/ < wm_keys.dconf
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < media_keys.dconf
+
