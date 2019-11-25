6177d3ced6f3e1cbc81c911d49c38dcdda932b75
yc.huang71 <yc.huang71@gmail.com>
2019-11-26 02:34:52 +0800

    add xfce config

--- backup.sh
+++ backup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 cp ~/.config/redshift.conf ./.config/
-cp ~/.config/Thunar ./.config
-cp ~/.config/xfce4/xfconf ./.config/xfce4/
+cp ~/.config/Thunar        ./.config/
+cp ~/.config/autostart     ./.config/
+
+cp ~/.config/xfce4/panel      ./.config/xfce4/
+cp ~/.config/xfce4/terminal   ./.config/xfce4/
+cp ~/.config/xfce4/xfconf     ./.config/xfce4/
 cp ~/.config/xfce4/helpers.rc ./.config/xfce4/
 
-cp ~/.vimrc ./
-cp ~/.tmux.conf ./
+cp ~/.vimrc      ./
+cp ~/.tmux.conf  ./
 cp ~/.Xresources ./
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cp .tmux.conf ~/
-cp .vimrc ~/
+cp .tmux.conf  ~/
+cp .vimrc      ~/
 cp .Xresources ~/
+
 cp ./.config/* ~/.config/ -r
