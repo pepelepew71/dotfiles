6107dea6f968579b88c57a8bc63c1d2aa9a49a56
yc.huang71 <yc.huang71@gmail.com>
2019-11-26 20:58:32 +0800

    mermer

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,4 +1,4 @@
-; thunar GtkAccelMap rc-file         -*- scheme -*-
+; Thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarLauncher/sendto-desktop" "")
@@ -56,6 +56,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-templates" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/copy" "<Primary>c")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-pathbar" "")
+; (gtk_accel_path "<Actions>/ThunarWindow/open-trash" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-descending" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/properties" "<Alt>Return")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
--- backup.sh
+++ backup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-cp ~/.config/redshift.conf ./.config/
-cp ~/.config/Thunar        ./.config/
-cp ~/.config/autostart     ./.config/
+cp ~/.config/redshift.conf ./.config/ -r
+cp ~/.config/Thunar        ./.config/ -r
+cp ~/.config/autostart     ./.config/ -r
 
-cp ~/.config/xfce4/panel      ./.config/xfce4/
-cp ~/.config/xfce4/terminal   ./.config/xfce4/
-cp ~/.config/xfce4/xfconf     ./.config/xfce4/
-cp ~/.config/xfce4/helpers.rc ./.config/xfce4/
+cp ~/.config/xfce4/panel      ./.config/xfce4/ -r
+cp ~/.config/xfce4/terminal   ./.config/xfce4/ -r
+cp ~/.config/xfce4/xfconf     ./.config/xfce4/ -r
+cp ~/.config/xfce4/helpers.rc ./.config/xfce4/ -r
 
 cp ~/.vimrc      ./
 cp ~/.tmux.conf  ./
