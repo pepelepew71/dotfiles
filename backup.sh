c790623251973a8ad4db620b991dc38881a4ae87
yc.huang71 <yc.huang71@gmail.com>
2020-03-11 00:07:27 +0800

    5 workspace

--- backup.sh
+++ backup.sh
@@ -2,7 +2,9 @@
 
 cp ~/.config/redshift.conf ./.config/ -r
 cp ~/.config/autostart     ./.config/ -r
-cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ./.config/xfce4//xfconf/xfce-perchannel-xml/ -r
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
 
 cp ~/.vimrc      ./
 cp ~/.tmux.conf  ./
