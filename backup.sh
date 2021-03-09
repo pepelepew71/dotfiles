9056bafbf8bc58ca27295f2a9e392826a791e26c
yc.huang71 <yc.huang71@gmail.com>
2021-03-09 12:16:09 +0800

    terminal and panel config two monitor

--- backup.sh
+++ backup.sh
@@ -4,7 +4,8 @@ cp ~/.config/redshift.conf ./.config/ -r
 cp ~/.config/autostart ./.config/ -r
 cp ~/.config/powerline-shell ./.config/ -r
 cp ~/.config/scripts ./.config/ -r
-cp ~/.config/terminal ./.config/ -r
+
+cp ~/.config/xfce4/terminal ./.config/xfce4 -r
 
 cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
 cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ./.config/xfce4/xfconf/xfce-perchannel-xml/ -r
